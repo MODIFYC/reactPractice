43bc35a8c2cd0f20b23953ae32529538087a4c13
MODIFYC <chsj5742@naver.com>
2022-03-08 22:34:40 +0900

    practice

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import styles from "./App.modules.css";
+
+function App() {
+    return (
+    <div>
+        <h1 className={styles.title}>hi</h1>
+        <Button text={"Continue"} />
+    </div>
+    )
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import PropTypes from 'prop-types';
+import App from "./App";
 
-function Btn({text, fontSize = 15}){  // == Btn(props)
-    return (
-        <button 
-        style={{
-            backgroundColor: "skyblue",
-            color: "white",
-            padding: "10px 20px",
-            border: 0,
-            borderRadius: 10,
-            fontSize,
-        }}>
-            {text} {/* == props.text */}
-        </button>
-    )
-}
-
-// 입력값 정의: propTypes
-//npm install --save prop-types
-Btn.propTypes = {
-    text: PropTypes.string.isRequired,  //isRequired: 필수값
-    fontSize: PropTypes.number,
-}
-
-function App() {
-    return (
-    <div>
-        <Btn text="Save Changes" fontSize={18} />
-        <Btn text="Continue" />
-    </div>
-    )
-}
-
-// export default App;
 ReactDOM.render(<App />, document.getElementById("root"));
