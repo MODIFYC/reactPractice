5a56ba363799655d453e675650344aa205e6237c
MODIFYC <chsj5742@naver.com>
2022-03-12 22:03:58 +0900

    practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import Button from "./Button";
 import styles from "./App.modules.css";
+import {useState, useEffect} from "react";
 
 function App() {
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev +1);
+    const onChange = (event) => setKeyword(event.target.value);
+    useEffect(() => {
+        console.log("CALL THE API....");
+    }, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes.");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes.");
+    }, [counter]);
+
     return (
     <div>
-        <h1 className={styles.title}>hi</h1>
-        <Button text={"Continue"} />
+        <h1>{counter}</h1>
+        <button onClick={onClick}>click me</button>
+        <hr />
+        <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
     </div>
     )
 }
